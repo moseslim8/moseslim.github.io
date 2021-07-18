064cc8da4b6835e93d541a54f211d652e30b8b78
Moses Lim <84589972+moseslim8@users.noreply.github.com>
2021-07-18 23:11:01 +1000

    Updated photography.js

--- docs/resources/js/photography.js
+++ docs/resources/js/photography.js
@@ -1,11 +1,12 @@
 var imgArray = [
     'resources/images/February2021-173.jpg',
     'resources/images/February2021-174.jpg',
-    '/resources/images/February2021-170.jpg',
-    'resources/images/January2021-28.jpg',
-    'resources/images/March2021-181.jpg',
+    'resources/images/February2021-170.jpg',
     'resources/images/January2021-220.jpg',
+    'resources/images/July2021-31.jpg',
+    'resources/images/July2021-35.jpg',
     'resources/images/February2021-196.jpg',
+    'resources/images/July2021-34.jpg',
     'resources/images/June2021-35.jpg',
     'resources/images/Mayl2021-105.jpg',
     'resources/images/November2020-38.jpg',
@@ -13,8 +14,8 @@ var imgArray = [
 ];
 
 var img = document.createElement('img');
-img.style.height = '25%';
-img.style.width = '60%';
+img.style.height = '100%';
+img.style.width = '100%';
 document.getElementById('slideshow').appendChild(img);
 
 var imageCounter = 0;
