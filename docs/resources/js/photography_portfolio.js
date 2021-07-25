70ca77bcc7daacac9a630d689930502f882ca177
Moses Lim <84589972+moseslim8@users.noreply.github.com>
2021-07-25 13:28:06 -0400

    Updated doc directory

---
+++ docs/resources/js/photography_portfolio.js
@@ -0,0 +1,30 @@
+var imgArray = [
+    'resources/images/February2021-173.jpg',
+    'resources/images/February2021-174.jpg',
+    'resources/images/February2021-170.jpg',
+    'resources/images/January2021-220.jpg',
+    'resources/images/July2021-31.jpg',
+    'resources/images/July2021-35.jpg',
+    'resources/images/February2021-196.jpg',
+    'resources/images/July2021-34.jpg',
+    'resources/images/June2021-35.jpg',
+    'resources/images/Mayl2021-105.jpg',
+    'resources/images/November2020-38.jpg',
+    'resources/images/April2021-191.jpg'
+];
+
+var img = document.createElement('img');
+img.style.width = '72%';
+img.style.padding = '1em';
+document.getElementById('slideshow').appendChild(img);
+
+var imageCounter = 0;
+function changePicture() {
+    img.src = imgArray[imageCounter];
+    imageCounter++;
+    if(imageCounter == imgArray.length) {
+        imageCounter = 0;    
+    }
+    
+    setTimeout("changePicture()", 3000);
+}
