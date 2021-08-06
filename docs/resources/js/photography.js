a12a75f33883b792b93b93105021ffec693480c3
Moses Lim <84589972+moseslim8@users.noreply.github.com>
2021-08-07 00:15:48 +1000

    Updated photography.js
    Added the ability to open and close modal windows

--- docs/resources/js/photography.js
+++ docs/resources/js/photography.js
@@ -1,3 +1,51 @@
+// Display modal window
+var modal = document.getElementsByClassName("modal-click");
+var link = document.getElementsByClassName("modal-link");
+var span = document.getElementsByClassName("close");
+var scrolling = document.getElementById("scrolling");
+
+link[0].onclick = function() {
+    modal[0].style.display = 'block';
+    link[0].style.color = 'rgba(255,255,255,0)';
+    
+    // Add class that removes the ability for the body to scroll when modal window is open
+    scrolling.classList.add('model-opened');
+}
+
+link[1].onclick = function() {
+    modal[1].style.display = 'block';
+    link[1].style.color = 'rgba(255,255,255,0)';
+    
+    // Add class that removes the ability for the body to scroll when modal window is open
+    scrolling.classList.add('model-opened');
+}
+
+span[0].onclick = function() {
+    modal[0].style.display = 'none';
+    link[0].style.color = null;
+    scrolling.classList.remove('model-opened');
+}
+
+span[1].onclick = function() {
+    modal[1].style.display = 'none';
+    link[1].style.color = null;
+    scrolling.classList.remove('model-opened');
+}
+
+window.onclick = function(event) {
+    if (event.target == modal[0]) {
+        modal[0].style.display = 'none';
+        link[0].style.color = null;
+        scrolling.classList.remove('model-opened');
+    }
+    
+    if (event.target == modal[1]) {
+        modal[1].style.display = 'none';
+        link[1].style.color = null;
+        scrolling.classList.remove('model-opened');
+    }
+}
+
 var imgArray = [
     'resources/images/February2021-173.jpg',
     'resources/images/February2021-174.jpg',
@@ -13,11 +61,14 @@ var imgArray = [
     'resources/images/April2021-191.jpg'
 ];
 
+// Creates an img tag for picture slideshow
 var img = document.createElement('img');
 img.style.width = '72%';
-img.style.padding = '1em';
+img.style.display = 'block';
+img.style.margin = '0 auto';
 document.getElementById('slideshow').appendChild(img);
 
+// Iterates through the array to display the images
 var imageCounter = 0;
 function changePicture() {
     img.src = imgArray[imageCounter];
